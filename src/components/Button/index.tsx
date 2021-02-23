75c5329257e1eb1cf0255d0713afd2e6e5374ad7
Daniel Silva <azsidaniel@gmail.com>
2021-02-22 22:03:53 -0300

    Cria componentes de Botão e Input e adiciona ícones.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+  onPress: () => void;
+}
+
+const Button: FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,12 +2,17 @@ import React, { FC } from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => false}>Entrar</Button>
     </Container>
   );
 };
